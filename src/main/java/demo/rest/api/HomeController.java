0bc977bcdfd3fdc4ae5a3eb5ec1b76add8cf052e
razerno <razerrno@gmail.com>
2017-08-23 21:12:05 +0100

    Add a home controller

---
+++ src/main/java/demo/rest/api/HomeController.java
@@ -0,0 +1,14 @@
+package demo.rest.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Offers can be accessed at /offers";
+    }
+}
